e7e63d9a4c1fb877653626ed93dc04968c870a09
hwijung <hwijung.ryu@gmail.com>
2015-11-22 14:37:05 +0900

    celeryd and celerybeat sh

--- PM/deplay_in_production.sh
+++ PM/deplay_in_production.sh
@@ -1,3 +1,5 @@
 # Copy this script to root directory you want to deploy and execute.
 wget http://www.github.com/hwijung/PM/archive/master.zip
 unzip master.zip
+
+
---
+++ PM/run_celerybeat.sh
@@ -0,0 +1,1 @@
+/etc/init.d/celerybeat restart
