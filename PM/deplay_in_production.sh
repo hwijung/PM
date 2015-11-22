b4598ac712bc396311811e23979277d554aaf9bf
hwijung <hwijung.ryu@gmail.com>
2015-11-22 14:51:28 +0900

    deploy script

--- PM/deplay_in_production.sh
+++ PM/deplay_in_production.sh
@@ -1,5 +1,11 @@
 # Copy this script to root directory you want to deploy and execute.
-wget http://www.github.com/hwijung/PM/archive/master.zip
+wget https://github.com/hwijung/PM/archive/master.zip
 unzip master.zip
+mv /home/hwijung/workspace/PM-master/PM /home/hwijung/workspace/PM
+rm -r PM-master
+rm master.zip
 
+# authorize directories
+chown www-data:www-data /home/hwijung/workspace/PM
+chown www-data:www-data /home/hwijung/workspace/PM/db.sqlite3
 
